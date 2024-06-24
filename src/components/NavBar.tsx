b6eefacb3a5e9210ca377ca2a804c83722a408fe
huynq2k4 <huynq.contact@gmail.com>
2024-06-24 10:09:16 +0700

    build

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,18 +1,4 @@
 import { Link } from 'react-router-dom';
-// import './NavBar.css';
-import React from 'react'
-
-const useViewport = () => {
-  const [width, setWidth] = React.useState(window.innerWidth);
-
-  React.useEffect(() => {
-    const handleWindowResize = () => setWidth(window.innerWidth);
-    window.addEventListener("resize", handleWindowResize);
-    return () => window.removeEventListener("resize", handleWindowResize);
-  }, []);
-
-  return { width };
-};
 
 function NavBar() {
   return (
